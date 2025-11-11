623b0aeaef129890e39fe8b98f2cec5119534dc9
dinethwick <161934270+dinethwick@users.noreply.github.com>
2025-11-11 17:56:49 +1030

    Set Vite base path for static hosting

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const redirectPath = sessionStorage.getItem('redirectPath')
+if (redirectPath) {
+  sessionStorage.removeItem('redirectPath')
+  const current = `${window.location.pathname}${window.location.search}${window.location.hash}`
+  if (redirectPath !== current) {
+    window.history.replaceState(null, '', redirectPath)
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
